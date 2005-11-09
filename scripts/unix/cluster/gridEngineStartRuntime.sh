415ae6da18efe31e503bf060d90d99e60067de76
(no author) <(no author)@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-11-09 16:48:46 +0000

    This commit was manufactured by cvs2svn to create tag 'PROACTIVE_3_0'.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/tags/PROACTIVE_3_0@3008 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/cluster/gridEngineStartRuntime.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for f in `cat $PE_HOSTFILE | cut -f1 -d " "`
-do
- echo "---"$f"---"
- ssh $f $* &
-done
-wait
--- src/nonregressiontest/component/interceptor/FooItf.java
+++
@@ -1,8 +0,0 @@
-package nonregressiontest.component.interceptor;
-
-public interface FooItf {
-    public static final String CLIENT_ITF_NAME = "foo-client";
-    public static final String SERVER_ITF_NAME = "foo-server";
-
-    public void foo();
-}
