47bd4a72f5d1eaffa001871e8caeca33b8b7931b
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-01-25 17:21:47 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_3.2@4398 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ext/scilab/gui/WelcomeDialog.java
+++ src/org/objectweb/proactive/ext/scilab/gui/WelcomeDialog.java
@@ -1,33 +1,33 @@
-/* 
+/*
  * ################################################################
- * 
- * ProActive: The Java(TM) library for Parallel, Distributed, 
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
- * 
+ *
  * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive@objectweb.org
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
- *  
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s): 
- * 
+ *  Contributor(s):
+ *
  * ################################################################
- */ 
+ */
 package org.objectweb.proactive.ext.scilab.gui;
 
 import java.awt.Dimension;
@@ -38,39 +38,39 @@ import java.awt.Toolkit;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
+
 public class WelcomeDialog extends JDialog {
-	private JPanel panelLogo = null ;
-	private Image logo;
-	public WelcomeDialog() {
-		super();
-		
-		logo = Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/logo.jpg"));
-		panelLogo = new JPanel (){
-			public void paintComponent ( Graphics g )
-			{
-				super.paintComponent(g) ;
+    private JPanel panelLogo = null;
+    private Image logo;
 
-				g.drawImage(logo, 0, 0, this);
-			}
-		};
-		
-		this.setSize(510, 300) ;
-		this.setResizable(false);
-		this.setTitle("Welcome");
-		this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-		this.setModal(false) ;
-		this.setUndecorated(true) ;
-		this.getContentPane().add(this.panelLogo) ;
-		this.center() ;
-		
-	}
+    public WelcomeDialog() {
+        super();
 
-	public void center() {
-		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension f = this.getSize();
+        logo = Toolkit.getDefaultToolkit()
+                      .getImage(getClass().getResource("img/logo.jpg"));
+        panelLogo = new JPanel() {
+                    public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+
+                        g.drawImage(logo, 0, 0, this);
+                    }
+                };
+
+        this.setSize(510, 300);
+        this.setResizable(false);
+        this.setTitle("Welcome");
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        this.setModal(false);
+        this.setUndecorated(true);
+        this.getContentPane().add(this.panelLogo);
+        this.center();
+    }
+
+    public void center() {
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension f = this.getSize();
         int x = (d.width - f.width) / 2;
         int y = (d.height - f.height) / 2;
-        this.setBounds(x, y, f.width, f.height );
-	}
-
+        this.setBounds(x, y, f.width, f.height);
+    }
 }
