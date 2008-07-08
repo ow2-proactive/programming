6e801b557928c827e77511b640ec2a5f5f57910c
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-08 13:33:41 +0000

    moved osgi examples to the new directory layout
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9628 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- examples/osgi/oscar.sh
+++ examples/osgi/oscar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 
+#
 # The following variable should be automatically
 # assigned during install, if not, edit it to reflect
 # your Java installation.
@@ -25,5 +25,5 @@ echo $MYHOSTNAME
 
 rm -rf cache/${MYHOSTNAME}/*
 
-${JAVA_HOME}/bin/java -Doscar.cache.profiledir="${OSCAR_HOME}/cache/${MYHOSTNAME}"  -Doscar.cache.profile="${MYHOSTNAME}"  -Djava.security.policy="${OSCAR_HOME}/etc/java.policy"  -Dcom.sun.management.jmxremote -Dproactive.http.servlet=true -Dproactive.communication.protocol=http -Dproactive.http.port=8080  -Doscar.system.properties="${OSCAR_HOME}/etc/system.properties"   -jar "$OSCAR_HOME/../../../lib/oscar.jar"
+${JAVA_HOME}/bin/java -Doscar.cache.profiledir="${OSCAR_HOME}/cache/${MYHOSTNAME}"  -Doscar.cache.profile="${MYHOSTNAME}"  -Djava.security.policy="${OSCAR_HOME}/etc/java.policy"  -Dcom.sun.management.jmxremote -Dproactive.http.servlet=true -Dproactive.communication.protocol=http -Dproactive.http.port=8080  -Doscar.system.properties="${OSCAR_HOME}/etc/system.properties"   -jar "$OSCAR_HOME/../../dist/lib/oscar.jar"
 #-Djava.security.manager
