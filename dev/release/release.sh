08a68c678d55e9dcad7d4c57a993ead606a3c522
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-09 21:36:28 +0000

    Create a zip file too
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9670 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- dev/release/release.sh
+++ dev/release/release.sh
@@ -102,3 +102,6 @@ rm -Rf doc-src/*_snippets/
 sed -i "s/{version}/$VERSION/" README.txt
 
 tar cvfz ${TMP}/ProActive-${VERSION}.tar.gz ${TMP_DIR}
+
+cd ${TMP}
+zip -r ProActive-${VERSION}.zip ProActive-${VERSION}
