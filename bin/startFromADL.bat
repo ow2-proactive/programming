9508e8aa559cc68d3c788973df8687e22b7d550b
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-10 15:13:42 +0000

    Add information about startFromADL.(sh|bat) PROACTIVE-428
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9687 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- bin/startFromADL.bat
+++ bin/startFromADL.bat
@@ -1,7 +1,12 @@
 @echo off
-echo.
-echo --- Fractal C3D example ----------------------------------------
-echo ---
+REM This script may be used to launch a Fractal/GCM component application 
+REM describe with an ADL.
+REM Usage:  
+REM startFromADL.sh descriptor_file fractal_ADL_file
+REM   descriptor_file   a deployment descriptor
+REM   fractal_ADL_file  a fractal ADL file describing your components assembly 
+REM                     (for an ADL file located in org/o/o/p/MyApp.fractal give
+REM                     org.o.o.p.MyApp as parameter)
 
 :doit
 SETLOCAL ENABLEDELAYEDEXPANSION
