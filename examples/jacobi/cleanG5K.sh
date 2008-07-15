0428c4608b80dd3333984b33e701f2e0ec4c8c46
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-15 14:27:39 +0000

    revert wrong commits
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9737 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- examples/jacobi/cleanG5K.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for  mach in $(cat ../../../descriptors/nodes.properties | sed 's/NODES=//' | tr ' ' '\n'|uniq ); 
-do 
-  echo $mach;
-  ssh $mach killall java &
-done
--- examples/jacobi/fillNodesProperties.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "NODES= $(oarstat -fj $1 | grep assigned_hostnames | sed 's/assigned_hostnames//' |tr '+=' ' ')" > ../../../descriptors/nodes.properties 
