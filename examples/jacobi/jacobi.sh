db8f4b49cda7b48c81fed9f7465d17c50b46c498
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-15 13:24:12 +0000

    descriptor and fix binding controller
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9729 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- examples/jacobi/jacobi.sh
+++ examples/jacobi/jacobi.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo
-echo --- Jacobi : nodes initialization -----------------------
-
 workingDir=`dirname $0`
 . ${workingDir}/../env.sh
-export XMLDESCRIPTOR=$workingDir/GCMA.xml
-$JAVACMD org.objectweb.proactive.examples.jacobi.Jacobi $XMLDESCRIPTOR
 
-echo
+JAVACMD=$JAVACMD" -Dfractal.provider=org.objectweb.proactive.core.component.Fractive"
+
+# ./jacobi.sh ../../../descriptors/MatrixOldDep.xml 100 multicast 2 2 100 100 
+
+#  -Djava.security.policy=../../../scripts/proactive.java.policy -Dlog4j.configuration=file:../../../scripts/proactive-log4j
+
+echo --- GCM application: Jacobi example ---------------------
+$JAVACMD -Dproactive.net.interface=eth0 org.objectweb.proactive.examples.components.jacobi.Jacobi $*
 echo ---------------------------------------------------------
