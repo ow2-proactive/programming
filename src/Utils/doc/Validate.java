4dd7e2c2f621a7bbeeaac05a09fb769dca5c5e97
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-21 08:44:07 +0000

    validation was not working properly,fixed
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9841 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Utils/doc/Validate.java
+++ src/Utils/doc/Validate.java
@@ -31,6 +31,12 @@
  */
 package doc;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -38,85 +44,88 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.ext.LexicalHandler;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.io.File;
-import java.io.FileInputStream;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-
-/** Check an xml file follows its dtd.  Greatly inspired from the apache xalan samples 'Validate'  */
+/**
+ * Check an xml file follows its dtd. Greatly inspired from the apache xalan
+ * samples 'Validate'
+ */
 public class Validate extends DefaultHandler implements LexicalHandler {
-    private String inputFile;
-    /** Flag set to false when the file is not well formed  */
-    boolean valid = true;
-    boolean hasDTD = false;
+    private final String inputFile;
+    /** Flag set to false when the file is not well formed */
+    private boolean valid = true;
+    private boolean hasDTD = false;
     StringBuffer errorBuffer;
 
-    Validate(String sourceFile, StringBuffer errorBuff) {
+    Validate(final String sourceFile, final StringBuffer errorBuff) {
         super();
         this.inputFile = sourceFile;
         this.errorBuffer = errorBuff;
+        this.parse();
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         if (args.length != 1) {
             System.out.println("Syntax:      java Validate fileName");
-            System.out
-                    .println("This parser tries compares an XML document to its dtd: is it conforming to its DOCTYPE?");
-
+            System.out.println("This parser tries compares an XML "
+                + "document to its dtd: is it conforming to its DOCTYPE?");
             return;
         }
-        StringBuffer errorBuffer = new StringBuffer();
+        final StringBuffer errorBuffer = new StringBuffer();
 
-        Validate validator = new Validate(args[0], errorBuffer);
+        final Validate validator = new Validate(args[0], errorBuffer);
 
-        System.out.print(validator.parse(args[0]));
-
-        if (!validator.isValid())
+        System.out.print(validator.parse());
+        if (!validator.isValid()) {
             throw new Error("The XML is not valid or files are missing.");
+        }
+
     }
 
-    /** Parse an XML file, and validate it.
-     * @return a buffer containing the errors (and a comment) */
-    private StringBuffer parse(String filename) {
-        StringBuffer returnedBuffer = new StringBuffer();
+    /**
+     * Parse an XML file, and validate it.
+     * 
+     * @return a buffer containing the errors (and a comment)
+     */
+    private StringBuffer parse() {
+        final StringBuffer returnedBuffer = new StringBuffer();
 
         try {
-            File f = new File(filename);
-            StringBuffer errorBuff = new StringBuffer();
-            InputSource input = new InputSource(new FileInputStream(f));
-            // Set systemID so parser can find the dtd with a relative URL in the source document.
+            final File f = new File(this.inputFile);
+            final InputSource input = new InputSource(new FileInputStream(f));
+            // Set systemID so parser can find the dtd with a relative URL in
+            // the source document.
             input.setSystemId(f.toString());
 
-            SAXParserFactory spfact = SAXParserFactory.newInstance();
+            final SAXParserFactory spfact = SAXParserFactory.newInstance();
 
             spfact.setValidating(true);
             spfact.setNamespaceAware(true);
 
-            SAXParser parser = spfact.newSAXParser();
+            final SAXParser parser = spfact.newSAXParser();
 
-            XMLReader reader = parser.getXMLReader();
+            final XMLReader reader = parser.getXMLReader();
+            // TODO WTF, instantiate a Validate inside the Validate ?
 
-            //Instantiate inner-class error and lexical handler.
-            Validate handler = new Validate(filename, errorBuff);
-            reader.setProperty("http://xml.org/sax/properties/lexical-handler", handler);
-            parser.parse(input, handler);
-
-            if (!handler.hasDTD) {
-                returnedBuffer.append("[WARNING] NO DOCTYPE DECLARATION in " + filename + "\n");
-            } else if (!handler.valid) {
-                returnedBuffer.append("[ERROR] XML is NOT VALID: it does not comply to dtd in " + filename +
-                    "\n");
-                returnedBuffer
-                        .append("[HINT] If you have trouble spotting exactly where the errors are, set util.DocBookize.SHORT_LINES to true.\n");
-                returnedBuffer.append(handler.errorBuffer);
+            // Instantiate inner-class error and lexical handler.
+            // Validate handler = new Validate(filename, errorBuff);
+            reader.setProperty("http://xml.org/sax/properties/lexical-handler", this);
+            parser.parse(input, this);
+            if (!this.hasDTD) {
+                returnedBuffer.append("[WARNING] NO DOCTYPE DECLARATION in " + this.inputFile + "\n");
+            } else if (!this.isValid()) {
+                returnedBuffer.append("[ERROR] XML is NOT VALID: it does not comply to dtd in " +
+                    this.inputFile + "\n");
+                returnedBuffer.append("[HINT] If you have " + "trouble spotting exactly where the errors "
+                    + "are, set util.DocBookize.SHORT_LINES to true.\n");
+                returnedBuffer.append(this.errorBuffer);
             } else {
-                returnedBuffer.append("[INFO] XML is VALID and complies to dtd (" + filename + ")\n");
+                returnedBuffer.append("[INFO] XML is VALID and complies to dtd (" + this.inputFile + ")\n");
             }
-        } catch (Exception e) // Any problem related to xml structure or file existence
+        } catch (final Exception e) // Any problem related to xml structure or
+        // file existence
         {
-            returnedBuffer.append("[ERROR] File NOT WELL-FORMED :" + filename + ". " + e.getMessage() + "\n");
+            returnedBuffer.append("[ERROR] File NOT WELL-FORMED :" + this.inputFile + ". " + e.getMessage() +
+                "\n");
         }
 
         return returnedBuffer;
@@ -124,8 +133,8 @@ public class Validate extends DefaultHandler implements LexicalHandler {
 
     /** When an error is encountered, store it for future recall */
     @Override
-    public void error(SAXParseException exc) {
-        String message = exc.getMessage();
+    public void error(final SAXParseException exc) {
+        final String message = exc.getMessage();
         this.errorBuffer.append("[ERROR] " + this.inputFile + "[" + exc.getLineNumber() + "] " + message +
             "\n");
 
@@ -138,24 +147,24 @@ public class Validate extends DefaultHandler implements LexicalHandler {
 
     /** When a warning is encountered, store it for future recall */
     @Override
-    public void warning(SAXParseException exc) {
+    public void warning(final SAXParseException exc) {
         this.errorBuffer.append("[WARNING] " + this.inputFile + "[" + exc.getLineNumber() + "] " +
             exc.getMessage() + "\n");
         this.valid = false;
     }
 
     /** Set hasDTD to true when dtd is found. */
-    public void startDTD(String name, String publicId, String systemId) throws SAXException {
+    public void startDTD(final String name, final String publicId, final String systemId) throws SAXException {
         this.hasDTD = true;
     }
 
     public void endDTD() throws SAXException {
     }
 
-    public void startEntity(String name) throws SAXException {
+    public void startEntity(final String name) throws SAXException {
     }
 
-    public void endEntity(String name) throws SAXException {
+    public void endEntity(final String name) throws SAXException {
     }
 
     public void startCDATA() throws SAXException {
@@ -164,10 +173,10 @@ public class Validate extends DefaultHandler implements LexicalHandler {
     public void endCDATA() throws SAXException {
     }
 
-    public void comment(char[] ch, int start, int length) throws SAXException {
+    public void comment(final char[] ch, final int start, final int length) throws SAXException {
     }
 
     public boolean isValid() {
-        return valid;
+        return this.valid;
     }
 }
