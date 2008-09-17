87f630e88e19536566d278b7a88753d920daa45a
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-09-17 14:28:10 +0000

    format
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@10179 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/ext/util/StubGenerator.java
+++ src/Core/org/objectweb/proactive/ext/util/StubGenerator.java
@@ -45,222 +45,211 @@ import javassist.ClassPool;
 import org.objectweb.proactive.core.mop.JavassistByteCodeStubBuilder;
 import org.objectweb.proactive.core.mop.Utils;
 
+
 public class StubGenerator {
-	public static void main(String[] args) {
-		StubGenerator sg = new StubGenerator(args);
-		sg.run();
-	}
+    public static void main(String[] args) {
+        StubGenerator sg = new StubGenerator(args);
+        sg.run();
+    }
 
-	private File srcDir;
-	private String pkg = "";
-	private File destDir;
-	private String cl;
-	private boolean verbose = false;
+    private File srcDir;
+    private String pkg = "";
+    private File destDir;
+    private String cl;
+    private boolean verbose = false;
 
-	public StubGenerator(String[] args) {
-		ClassPool pool = ClassPool.getDefault();
-		pool.insertClassPath(new ClassClassPath(this.getClass()));
+    public StubGenerator(String[] args) {
+        ClassPool pool = ClassPool.getDefault();
+        pool.insertClassPath(new ClassClassPath(this.getClass()));
 
-		int index = 0;
-		while (index < args.length) {
-			if (args[index].equals("-srcDir")) {
-				srcDir = new File(args[index + 1]);
-				index += 2;
-			} else if (args[index].equals("-pkg")) {
-				pkg = args[index + 1];
-				index += 2;
-			} else if (args[index].equals("-destDir")) {
-				destDir = new File(args[index + 1]);
-				index += 2;
-			} else if (args[index].equals("-class")) {
-				cl = args[index + 1];
-				index += 2;
-			} else if (args[index].equals("-verbose")) {
-				verbose = true;
-				index++;
-			} else {
-				usage();
-				System.exit(1);
-			}
-		}
-	}
+        int index = 0;
+        while (index < args.length) {
+            if (args[index].equals("-srcDir")) {
+                srcDir = new File(args[index + 1]);
+                index += 2;
+            } else if (args[index].equals("-pkg")) {
+                pkg = args[index + 1];
+                index += 2;
+            } else if (args[index].equals("-destDir")) {
+                destDir = new File(args[index + 1]);
+                index += 2;
+            } else if (args[index].equals("-class")) {
+                cl = args[index + 1];
+                index += 2;
+            } else if (args[index].equals("-verbose")) {
+                verbose = true;
+                index++;
+            } else {
+                usage();
+                System.exit(1);
+            }
+        }
+    }
 
-	public void usage() {
-		System.out.println("Usage:");
-		System.out.println("\t-srcDir  directory where to find source classes");
-		System.out.println("\t-destDir directory where to put generated stubs");
-		System.out.println("\t-pkg     package name");
-		System.out.println("\t-class   generate only a stub for this class");
-		System.out.println("\t-verbose enable verbose mode");
-		System.out.println("");
-	}
+    public void usage() {
+        System.out.println("Usage:");
+        System.out.println("\t-srcDir  directory where to find source classes");
+        System.out.println("\t-destDir directory where to put generated stubs");
+        System.out.println("\t-pkg     package name");
+        System.out.println("\t-class   generate only a stub for this class");
+        System.out.println("\t-verbose enable verbose mode");
+        System.out.println("");
+    }
 
-	public void logAndExit(String str) {
-		System.err.println(str);
-		System.exit(2);
-	}
+    public void logAndExit(String str) {
+        System.err.println(str);
+        System.exit(2);
+    }
 
-	public void run() {
-		if (srcDir == null) {
-			logAndExit("srcDir attribute is not set");
-		}
-		if (!srcDir.exists()) {
-			logAndExit("Invalid srcDir attribute: " + srcDir.toString()
-					+ " does not exist");
-		}
-		if (!srcDir.isDirectory()) {
-			logAndExit("Invalid srcDir attribute: " + srcDir.toString()
-					+ " is not a directory");
-		}
+    public void run() {
+        if (srcDir == null) {
+            logAndExit("srcDir attribute is not set");
+        }
+        if (!srcDir.exists()) {
+            logAndExit("Invalid srcDir attribute: " + srcDir.toString() + " does not exist");
+        }
+        if (!srcDir.isDirectory()) {
+            logAndExit("Invalid srcDir attribute: " + srcDir.toString() + " is not a directory");
+        }
 
-		if (pkg == null) {
-			logAndExit("pkg attribute is not set");
-		}
-		File pkgDir = new File(srcDir.toString() + File.separator
-				+ pkg.replace('.', File.separatorChar));
-		if (!pkgDir.exists()) {
-			logAndExit("Invalid pkg attribute: " + pkgDir.toString()
-					+ " does not exist");
-		}
+        if (pkg == null) {
+            logAndExit("pkg attribute is not set");
+        }
+        File pkgDir = new File(srcDir.toString() + File.separator + pkg.replace('.', File.separatorChar));
+        if (!pkgDir.exists()) {
+            logAndExit("Invalid pkg attribute: " + pkgDir.toString() + " does not exist");
+        }
 
-		if (destDir == null) {
-			destDir = srcDir;
-		}
-		if (!destDir.isDirectory()) {
-			logAndExit("Invalid dest attribute: " + destDir.toString()
-					+ " is not a directory");
-		}
-		if (!destDir.isDirectory()) {
-			logAndExit("Invalid src attribute: " + destDir.toString()
-					+ " is not a directory");
-		}
+        if (destDir == null) {
+            destDir = srcDir;
+        }
+        if (!destDir.isDirectory()) {
+            logAndExit("Invalid dest attribute: " + destDir.toString() + " is not a directory");
+        }
+        if (!destDir.isDirectory()) {
+            logAndExit("Invalid src attribute: " + destDir.toString() + " is not a directory");
+        }
 
-		List<File> files = new ArrayList<File>();
+        List<File> files = new ArrayList<File>();
 
-		if (cl == null) {
-			// Find all the classes in this package
-			files.addAll(exploreDirectory(pkgDir));
-		} else {
-			File file = new File(pkgDir + File.separator + cl + ".class");
-			if (!file.exists() || !file.isFile()) {
-				logAndExit("Invalid pkg or class attribute: " + file.toString()
-						+ " does not exist");
-			}
+        if (cl == null) {
+            // Find all the classes in this package
+            files.addAll(exploreDirectory(pkgDir));
+        } else {
+            File file = new File(pkgDir + File.separator + cl + ".class");
+            if (!file.exists() || !file.isFile()) {
+                logAndExit("Invalid pkg or class attribute: " + file.toString() + " does not exist");
+            }
 
-			files.add(file);
-		}
+            files.add(file);
+        }
 
-		PrintStream stderr = System.err;
-		PrintStream mute = new PrintStream(new MuteOutputStream());
+        PrintStream stderr = System.err;
+        PrintStream mute = new PrintStream(new MuteOutputStream());
 
-		if (!verbose) {
-			System.setErr(mute);
-		}
+        if (!verbose) {
+            System.setErr(mute);
+        }
 
-		// ClassPool.releaseUnmodifiedClassFile = true;
-		for (File file : files) {
-			String str = file.toString().replaceFirst(
-					srcDir.toString().replace("\\", "\\\\"), "");
-			try {
-				if (!verbose) {
-					System.setErr(mute);
-				}
+        // ClassPool.releaseUnmodifiedClassFile = true;
+        for (File file : files) {
+            String str = file.toString().replaceFirst(srcDir.toString().replace("\\", "\\\\"), "");
+            try {
+                if (!verbose) {
+                    System.setErr(mute);
+                }
 
-				StubGenerator.generateClass(str, destDir.toString()
-						+ File.separator);
-			} catch (Throwable e) {
-				System.out.println("Stub generation failed: " + str);
-			}
-		}
+                StubGenerator.generateClass(str, destDir.toString() + File.separator);
+            } catch (Throwable e) {
+                System.out.println("Stub generation failed: " + str);
+            }
+        }
 
-		if (!verbose) {
-			System.setErr(stderr);
-		}
-	}
+        if (!verbose) {
+            System.setErr(stderr);
+        }
+    }
 
-	private List<File> exploreDirectory(File dir) {
-		List<File> files = new ArrayList<File>();
+    private List<File> exploreDirectory(File dir) {
+        List<File> files = new ArrayList<File>();
 
-		for (File file : dir.listFiles()) {
-			if (file.isDirectory()) {
-				files.addAll(exploreDirectory(file));
-			}
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                files.addAll(exploreDirectory(file));
+            }
 
-			if (!file.toString().endsWith(".class")) {
-				continue;
-			}
+            if (!file.toString().endsWith(".class")) {
+                continue;
+            }
 
-			files.add(file);
-		}
+            files.add(file);
+        }
 
-		return files;
-	}
+        return files;
+    }
 
-	/**
-	 * @param arg
-	 * @param directoryName
-	 */
-	public static void generateClass(String arg, String directoryName) {
-		String className = processClassName(arg);
-		String fileName = null;
+    /**
+     * @param arg
+     * @param directoryName
+     */
+    public static void generateClass(String arg, String directoryName) {
+        String className = processClassName(arg);
+        String fileName = null;
 
-		String stubClassName = null;
+        String stubClassName = null;
 
-		try {
-			// Generates the bytecode for the class
-			byte[] data;
+        try {
+            // Generates the bytecode for the class
+            byte[] data;
 
-			data = JavassistByteCodeStubBuilder.create(className, null);
-			stubClassName = Utils.convertClassNameToStubClassName(className,
-					null);
+            data = JavassistByteCodeStubBuilder.create(className, null);
+            stubClassName = Utils.convertClassNameToStubClassName(className, null);
 
-			char sep = File.separatorChar;
-			fileName = directoryName + stubClassName.replace('.', sep)
-					+ ".class";
+            char sep = File.separatorChar;
+            fileName = directoryName + stubClassName.replace('.', sep) + ".class";
 
-			// And writes it to a file
-			new File(fileName.substring(0, fileName.lastIndexOf(sep))).mkdirs();
+            // And writes it to a file
+            new File(fileName.substring(0, fileName.lastIndexOf(sep))).mkdirs();
 
-			// String fileName = directoryName + System.getProperty
-			// ("file.separator") +
-			File f = new File(fileName);
-			FileOutputStream fos = new FileOutputStream(f);
-			fos.write(data);
-			fos.flush();
-			fos.close();
-		} catch (Exception e) {
-			System.err.println("Cannot compute stub generation for class "
-					+ className);
-			System.err.println("Reason is " + e);
-		}
-	}
+            // String fileName = directoryName + System.getProperty
+            // ("file.separator") +
+            File f = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(data);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            System.err.println("Cannot compute stub generation for class " + className);
+            System.err.println("Reason is " + e);
+        }
+    }
 
-	/**
-	 * Turn a file name into a class name if necessary. Remove the ending .class
-	 * and change all the '/' into '.'
-	 * 
-	 * @param name
-	 */
-	protected static String processClassName(String name) {
-		int i = name.indexOf(".class");
-		String tmp = name;
-		if (i < 0) {
-			return name;
-		}
-		tmp = name.substring(0, i);
+    /**
+     * Turn a file name into a class name if necessary. Remove the ending .class
+     * and change all the '/' into '.'
+     * 
+     * @param name
+     */
+    protected static String processClassName(String name) {
+        int i = name.indexOf(".class");
+        String tmp = name;
+        if (i < 0) {
+            return name;
+        }
+        tmp = name.substring(0, i);
 
-		String tmp2 = tmp.replace(File.separatorChar, '.');
+        String tmp2 = tmp.replace(File.separatorChar, '.');
 
-		if (tmp2.indexOf('.') == 0) {
-			return tmp2.substring(1);
-		}
-		return tmp2;
-	}
+        if (tmp2.indexOf('.') == 0) {
+            return tmp2.substring(1);
+        }
+        return tmp2;
+    }
 
-	public class MuteOutputStream extends OutputStream {
-		@Override
-		public void write(int b) throws IOException {
-			// Please shut up !
-		}
-	}
+    public class MuteOutputStream extends OutputStream {
+        @Override
+        public void write(int b) throws IOException {
+            // Please shut up !
+        }
+    }
 }
