789217726bb6ec401800e266be63710d1f70cf44
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-09-19 15:46:35 +0000

    Jalope
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@10194 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/mpi/control/NativeData.java
+++ src/Core/org/objectweb/proactive/mpi/control/NativeData.java
@@ -2,20 +2,21 @@ package org.objectweb.proactive.mpi.control;
 
 import java.io.Serializable;
 
+
 public class NativeData implements Serializable {
 
     private byte[] data;
-    
-	public NativeData() {
-		
-	}
 
-	public byte[] getData() {
-		return data;
-	}
+    public NativeData() {
 
-	public void setData(byte[] data) {
-		this.data = data;
-	}
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
 
 }
