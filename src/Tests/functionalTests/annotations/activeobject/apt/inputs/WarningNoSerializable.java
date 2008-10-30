dde4f75fd79ac4790a4da3e23b1165ee30e0db9d
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-10-30 11:46:22 +0000

    Undone #10402 Something really bad happened to the Compile_Time_Annonation branch or to git-svn.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@10403 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/annotations/activeobject/apt/inputs/WarningNoSerializable.java
+++
@@ -1,8 +0,0 @@
-package functionalTests.annotations.activeobject.apt.inputs;
-
-import org.objectweb.proactive.extra.annotation.activeobject.ActiveObject;
-
-@ActiveObject
-public class WarningNoSerializable {
-
-}
