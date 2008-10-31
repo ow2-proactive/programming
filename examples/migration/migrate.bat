254b360c990e47da86d5e31beaef75df1a229a38
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-10-31 10:44:55 +0000

    fix for PROACTIVE-422, previous fix contained a typo error
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@10460 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- examples/migration/migrate.bat
+++ examples/migration/migrate.bat
@@ -8,9 +8,9 @@ goto doit
 
 :usage
 echo  Migrate an ActiveObject from Node1 to Node2
-echo  migrate.sh <the url of the source node> <the url of the destination node>
-echo      ex : migrate.bat  rmi://$HOSTNAME/Node1 rmi://$HOSTNAME/Node2
-echo      ex : migrate.bat jini://$HOSTNAME/Node1 rmi://$HOSTNAME/Node2
+echo  migrate.bat the_url_of_the_source_node the_url_of_the_destination_node
+echo      ex : migrate.bat  rmi://%HOSTNAME%/Node1 rmi://%HOSTNAME%/Node2
+echo      ex : migrate.bat jini://%HOSTNAME%/Node1 rmi://%HOSTNAME%/Node2
 echo.
 echo.
 goto end
