11164c935aec6c59477ae67c872f5f21904df809
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-03-18 09:52:36 +0000

    ProActiveLogCollector is a remote object, not an active object
    Removed useless interfaces and the initActive() method
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@11587 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/util/log/remote/ProActiveLogCollector.java
+++ src/Core/org/objectweb/proactive/core/util/log/remote/ProActiveLogCollector.java
@@ -1,14 +1,10 @@
 package org.objectweb.proactive.core.util.log.remote;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
-import org.objectweb.proactive.Body;
-import org.objectweb.proactive.InitActive;
-import org.objectweb.proactive.api.PAActiveObject;
 
 
 /**
@@ -18,16 +14,8 @@ import org.objectweb.proactive.api.PAActiveObject;
  * collector which given them to the correct logger/appender. So, all the log4j
  * events of the application are handled by the same hierarchy on the same machine.
  */
-@SuppressWarnings("serial")
-public class ProActiveLogCollector implements Serializable, InitActive {
+public class ProActiveLogCollector {
     final private ConcurrentHashMap<String, Logger> loggers = new ConcurrentHashMap<String, Logger>();
-
-    public ProActiveLogCollector() {
-    }
-
-    public void initActivity(Body body) {
-        PAActiveObject.setImmediateService("sendMessage");
-    }
 
     public void sendEvent(List<LoggingEvent> events) {
         for (LoggingEvent event : events) {
