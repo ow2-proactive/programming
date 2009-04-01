75eb6126de95a8902ad9f29e5e519a5f4896c62d
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-01 12:46:40 +0000

    PROACTIVE-637: Fixed Length of the protocol name.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@11761 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/config/ProActiveConfiguration.java
+++ src/Core/org/objectweb/proactive/core/config/ProActiveConfiguration.java
@@ -172,7 +172,6 @@ public class ProActiveConfiguration {
 
         @Override
         protected void finalize() throws Throwable {
-            logger.warn("FINALIZE CALLED");
 
             for (String key : exportedKeys.keySet()) {
                 System.setProperty(key, exportedKeys.get(key));
@@ -250,7 +249,7 @@ public class ProActiveConfiguration {
             fname = PROACTIVE_USER_CONFIG_FILENAME;
         }
 
-        if (!fname.matches("^\\w+:.*$")) {
+        if (!fname.matches("^\\w{2,}+:.*$")) {
             // protocol prefix was not specified
             // using "file" protocol by default
             fname = FILE_PROTOCOL_PREFIX + fname;
