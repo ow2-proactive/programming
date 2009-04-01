09277210cd9ad83d09a6002a12246daa1bf95248
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-01 14:35:05 +0000

    Update ProActive Programming version to 4.0.4
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_4.0.x@11764 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 Thanks for your interest in ProActive.
 
-ProActive 4.0.3
+ProActive 4.0.4
 
 You can find the documentation of ProActive in the docs directory:
 
--- src/Core/org/objectweb/proactive/Main.java
+++ src/Core/org/objectweb/proactive/Main.java
@@ -45,7 +45,7 @@ public class Main {
      * @return String
      */
     public static String getProActiveVersion() {
-        return "4.0.3";
+        return "4.0.4";
     }
 
     public static void main(String[] args) {
