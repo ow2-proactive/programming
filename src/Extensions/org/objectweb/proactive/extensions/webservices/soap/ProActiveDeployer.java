8523d45e1db46c55a4342714c6846d2edbdf216b
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-08-03 14:44:14 +0000

    resolve PROACTIVE-716:remove debug traces in ProActiveProvider
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@13727 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/webservices/soap/ProActiveDeployer.java
+++ src/Extensions/org/objectweb/proactive/extensions/webservices/soap/ProActiveDeployer.java
@@ -227,12 +227,10 @@ public class ProActiveDeployer extends WSConstants {
         props.put(WSDL_FILE, wsdl);
 
         if (componentInterface) {
-            logger.info("INTERFACE");
             props.put(COMPONENT_INTERFACE, "true");
             props.put(PROACTIVE_STUB, HttpMarshaller.marshallObject(o));
 
         } else {
-            logger.info("PAS INTERFACE");
             props.put(COMPONENT_INTERFACE, "false");
             props.put(PROACTIVE_STUB, HttpMarshaller.marshallObject(o));
 
