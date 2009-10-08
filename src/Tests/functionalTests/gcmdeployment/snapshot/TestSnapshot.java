0c6408c5ed825f196274ba8ca9adea625c4cd622
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-08 12:12:55 +0000

    PROACTIVE-732: Updated TestSnapshot to make it work with PAMR
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@14332 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/gcmdeployment/snapshot/TestSnapshot.java
+++ src/Tests/functionalTests/gcmdeployment/snapshot/TestSnapshot.java
@@ -10,8 +10,8 @@ import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.api.PAFuture;
 import org.objectweb.proactive.api.PALifeCycle;
 import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.config.PAProperties;
 import org.objectweb.proactive.core.node.Node;
-import org.objectweb.proactive.core.node.NodeFactory;
 import org.objectweb.proactive.core.runtime.RuntimeFactory;
 import org.objectweb.proactive.core.util.Sleeper;
 import org.objectweb.proactive.core.xml.VariableContractImpl;
@@ -82,6 +82,11 @@ public class TestSnapshot extends GCMFunctionalTestDefaultNodes {
             VariableContractImpl vc = new VariableContractImpl();
             vc.setVariableFromProgram(GCMFunctionalTestDefaultNodes.VAR_HOSTCAPACITY, Integer.valueOf(NB_VMS)
                     .toString(), VariableContractType.DescriptorDefaultVariable);
+            String value = PAProperties.PA_NET_ROUTER_PORT.getCmdLine() +
+                PAProperties.PA_NET_ROUTER_PORT.getValue() + " " +
+                PAProperties.PA_NET_ROUTER_ADDRESS.getCmdLine() + "localhost";
+            vc.setVariableFromProgram(GCMFunctionalTestDefaultNodes.VAR_JVMARG, value,
+                    VariableContractType.DescriptorDefaultVariable);
             File f = new File(this.getClass().getResource("/functionalTests/_CONFIG/JunitApp.xml").getFile());
             GCMApplication app = PAGCMDeployment.loadApplicationDescriptor(f, vc);
             app.startDeployment();
