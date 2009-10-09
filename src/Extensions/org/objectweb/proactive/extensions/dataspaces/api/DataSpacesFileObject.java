cfcd6ab180657f37c43adadb63bc953de1e6caf1
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-09 12:49:15 +0000

    Fixed broken build
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@14344 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/dataspaces/api/DataSpacesFileObject.java
+++ src/Extensions/org/objectweb/proactive/extensions/dataspaces/api/DataSpacesFileObject.java
@@ -29,10 +29,12 @@
  * ################################################################
  * $$PROACTIVE_INITIAL_DEV$$
  */
-package are highly inspired by Apache Commons VFS API,
- * as implementations of these interfaces are/were adapted to VFS interfaces. 
- */
 package org.objectweb.proactive.extensions.dataspaces.api;
+
+/*
+ * This interface and some other interfaces in this package are highly inspired by Apache Commons VFS API,
+ * package are highly inspired by Apache Commons VFS API,
+ */
 
 import java.util.List;
 
