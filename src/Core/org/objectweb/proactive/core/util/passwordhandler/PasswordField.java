0c83e7242c2735bde1e8e767fbf298e22c084c75
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-12 13:30:27 +0000

    Rewritten PasswordField
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@14385 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/util/passwordhandler/PasswordField.java
+++ src/Core/org/objectweb/proactive/core/util/passwordhandler/PasswordField.java
@@ -35,6 +35,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 /**
@@ -45,30 +46,44 @@ import java.util.Arrays;
  * @since ProActive 4.0
  */
 public class PasswordField {
+    /**
+     * Get the password as entered by the user on the given input stream.
+     * 
+     * @param input The input stream
+     * @param prompt The prompt to display to the user.
+     * @return The password as entered by the user.
+     */
+    public static final char[] getPassword(InputStream in, String promt) throws IOException {
+        PushbackInputStream pbIn = null;
+        if (pbIn instanceof PushbackInputStream) {
+            pbIn = (PushbackInputStream) in;
+        } else {
+            pbIn = new PushbackInputStream(in);
+        }
+
+        return getPassword(pbIn, promt);
+    }
 
     /**
      * Get the password as entered by the user on the given input stream.
      * 
-     * @param input stream to be used (e.g. System.in)
+     * @param input stream to be used
      * @param prompt The prompt to display to the user.
      * @return The password as entered by the user.
      */
-    public static final char[] getPassword(InputStream in, String prompt) throws IOException {
+    public static final char[] getPassword(PushbackInputStream in, String prompt) throws IOException {
         MaskingThread maskingthread = new MaskingThread(prompt);
-        Thread thread = new Thread(maskingthread);
-        thread.start();
+        maskingthread.start();
 
-        char[] lineBuffer;
-        char[] buf;
-
-        buf = lineBuffer = new char[128];
+        char[] buf = new char[64];
 
         int room = buf.length;
         int offset = 0;
         int c;
 
         loop: while (true) {
-            switch (c = in.read()) {
+            c = in.read();
+            switch (c) {
                 case -1:
                 case '\n':
                     break loop;
@@ -76,27 +91,24 @@ public class PasswordField {
                 case '\r':
                     int c2 = in.read();
                     if ((c2 != '\n') && (c2 != -1)) {
-                        if (!(in instanceof PushbackInputStream)) {
-                            in = new PushbackInputStream(in);
-                        }
-                        ((PushbackInputStream) in).unread(c2);
+                        in.unread(c2);
                     } else {
                         break loop;
                     }
 
                 default:
                     if (--room < 0) {
-                        buf = new char[offset + 128];
-                        room = buf.length - offset - 1;
-                        System.arraycopy(lineBuffer, 0, buf, 0, offset);
-                        Arrays.fill(lineBuffer, ' ');
-                        lineBuffer = buf;
+                        char[] tmp = new char[offset + 64];
+                        room = tmp.length - offset - 1;
+                        System.arraycopy(buf, 0, tmp, 0, offset);
+                        Arrays.fill(buf, ' ');
+                        buf = tmp;
                     }
                     buf[offset++] = (char) c;
                     break;
             }
         }
-        maskingthread.stopMasking();
+        maskingthread.terminate();
         if (offset == 0) {
             return null;
         }
@@ -104,5 +116,40 @@ public class PasswordField {
         System.arraycopy(buf, 0, ret, 0, offset);
         Arrays.fill(buf, ' ');
         return ret;
+    }
+
+    private static class MaskingThread extends Thread {
+        private AtomicBoolean cont = new AtomicBoolean(true);
+
+        /**
+         * Create new masking thread.
+         * 
+         *@param prompt The prompt displayed to the user
+         */
+        public MaskingThread(String prompt) {
+            System.out.print(prompt + " ");
+        }
+
+        /**
+         * Begin masking until asked to stop.
+         */
+        public void run() {
+
+            int oldPrio = Thread.currentThread().getPriority();
+            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+
+            try {
+                while (this.cont.get()) {
+                    System.out.print("\010 ");
+                    Thread.yield();
+                }
+            } finally { // restore the original priority
+                Thread.currentThread().setPriority(oldPrio);
+            }
+        }
+
+        public void terminate() {
+            this.cont.set(false);
+        }
     }
 }
