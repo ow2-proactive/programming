084bac6e9dc3dd04d0e1fe4ae989219fb4832546
ffonteno <ffonteno@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-04-14 11:39:37 +0000

    Removed java.util.ServiceConfigurationError import: java.util.ServiceConfigurationError is not Java 5 compliant (and not used)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@15930 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/config/PAProperties.java
+++ src/Core/org/objectweb/proactive/core/config/PAProperties.java
@@ -43,7 +43,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.ServiceConfigurationError;
 
 import javax.imageio.spi.ServiceRegistry;
 
