b26c25de6f11933a6802fd5e2f734f3f7d724b0f
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-10-15 09:35:52 +0000

    getProActiveVersion() now returns MAJOR_VERSION-dev in the trunk
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@17692 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/Main.java
+++ src/Core/org/objectweb/proactive/Main.java
@@ -54,7 +54,7 @@ public class Main {
      * @return String
      */
     public static String getProActiveVersion() {
-        return "2008-07-10 11:59:18";
+        return "4.4.0-dev";
     }
 
     public static void main(String[] args) {
