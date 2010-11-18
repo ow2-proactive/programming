da67a0f0887c71528cfe53df6b2bd5ea801154e8
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-11-18 16:55:03 +0000

    PROACTIVE-943: Remove e.printStackStrace() if roe.createRemoteObject fails
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@17930 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/body/AbstractUniversalBody.java
+++ src/Core/org/objectweb/proactive/core/body/AbstractUniversalBody.java
@@ -120,7 +120,6 @@ public abstract class AbstractUniversalBody implements UniversalBody, Serializab
             RemoteRemoteObject rro = this.roe.createRemoteObject(this.bodyID.toString(), false);
             this.remoteBody = (UniversalBody) new RemoteObjectAdapter(rro).getObjectProxy();
         } catch (Exception e) {
-            e.printStackTrace();
             throw new ActiveObjectCreationException(e);
         }
     }
