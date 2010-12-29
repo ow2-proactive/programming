de6f29beffdaeefef2182992be5b8b7463a7cb1d
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-12-29 10:42:46 +0000

    PROACTIVE-891: Jalope
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@18304 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/util/ProActiveRandom.java
+++ src/Core/org/objectweb/proactive/core/util/ProActiveRandom.java
@@ -48,7 +48,7 @@ import java.security.SecureRandom;
  */
 public class ProActiveRandom {
     final static private SecureRandom prng = new SecureRandom();
-    
+
     static private char[] symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
 
     /** Returns the next pseudorandom, uniformly distributed boolean value from this random number generator's sequence. */
