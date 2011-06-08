5ed3a82497b5083fe6ad1c79448be842fa8af667
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-06-08 16:09:02 +0000

    PROACTIVE-1040: Add RefactorWhenDroppingJava5 annotation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@19714 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Core/org/objectweb/proactive/utils/RefactorWhenDroppingJava5.java
@@ -0,0 +1,63 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2010 INRIA/University of 
+ * 				Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 
+ * or a different license than the GPL.
+ *
+ *  Initial developer(s):               The ActiveEon Team
+ *                        http://www.activeeon.com/
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $ACTIVEEON_INITIAL_DEV$
+ */
+package org.objectweb.proactive.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * A marker to identify non optimal code intended to support Java 5.
+ * 
+ * This marker helps to identify following code:
+ * <ul>
+ *  <li>Code that could be prettier if Java 6 API was used instead of Java 5 </li>
+ *  <li>Code that use a degraded mode for Java 5</li>
+ *  <li>Code that compile with Java 5 but does not work at runtime</li> 
+ * </ul>
+ * 
+ * @author ProActive team
+ * @since  ProActive 5.1.0
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target( { ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE, ElementType.METHOD,
+        ElementType.PARAMETER })
+public @interface RefactorWhenDroppingJava5 {
+
+}
