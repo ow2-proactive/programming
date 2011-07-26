03d445c2efcff20f2a96906421d1f92b42775982
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-07-26 11:46:50 +0000

    PROACTIVE-1113: Update release script to build src and bin artifacts
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@20373 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- dev/release/build_release.sh
+++ dev/release/build_release.sh
@@ -1,27 +1,10 @@
 #! /bin/sh
 
-TMP=/tmp
+function die {
+	echo "BUILD FAILED: $1" 1>&2
+	exit 2
+}
 
-# /mnt/scratch is a tmpfs mount point for faster builds on schubby
-if [ -w "/mnt/scratch" ] ; then
-	TMP=/mnt/scratch
-fi
-
-PROACTIVE_DIR=$1
-VERSION=$2
-JAVA_HOME=$3
-if [ ! -z "$4" ] ; then
-	TMP=$4
-fi
-
-
-TMP_DIR=""
-DIR_NAME="ProActiveProgramming-${VERSION}_core"
-
-echo " [i] PROACTIVE_DIR: $PROACTIVE_DIR"
-echo " [i] VERSION:       $VERSION"
-echo " [i] JAVA_HOME:     $JAVA_HOME"
-echo " [i] TMP:           $TMP"
 function warn_and_exit {
 	echo "$1" 1>&2
 	exit 1
@@ -34,9 +17,24 @@ function warn_print_usage_and_exit {
 	exit 1
 }
 
+##########
+# Fetch arguments
 
-if [ -z "$PROACTIVE_DIR" ] ; then
-	warn_print_usage_and_exit "PROACTIVE_DIR is not defined"
+# Source directory (should be a clean checkout) 
+ORIG_DIR=$1
+# Version to release (can be x.y.z ot x.y.z-tag)
+VERSION=$2
+# The JDK to use to build the release
+JAVA_HOME=$3
+# Where to put the artifacts
+ARTIFACT_DIR=$4
+# All work happen in this directory
+WORK_DIR=$(mktemp -d)
+# Eeach artifact use this directory as a master then strip or enhance it
+MASTER_DIR=${WORK_DIR}/master
+
+if [ -z "$ORIG_DIR" ] ; then
+	warn_print_usage_and_exit "ORIG_DIR is not defined"
 fi
 
 if [ -z "$VERSION" ] ; then
@@ -46,80 +44,108 @@ fi
 if [ -z "$JAVA_HOME" ] ; then
 	warn_print_usage_and_exit "JAVA_HOME is not defined"
 fi
+if [ -z "$ARTIFACT_DIR" ] ; then
+	warn_print_usage_and_exit "ARTIFACT_DIR is not defined"
+fi
 export JAVA_HOME=${JAVA_HOME}
 
 
-TMP_DIR="${TMP}/${DIR_NAME}"
-output=$(mkdir "${TMP_DIR}" 2>&1)
-if [ "$?" -ne 0 ] ; then
-	if [ -e ${TMP_DIR} ] ; then
-		echo " [w] ${TMP_DIR} already exists. Delete it !"
-		rm -Rf ${TMP_DIR}
-		mkdir ${TMP_DIR}
-		if [ "$?" -ne 0 ] ; then
-			warn_and_exit "Cannot create ${TMP_DIR}: $output"
-		fi
-	else
-		warn_and_exit "Cannot create ${TMP_DIR}"
-	fi
-fi
+###########
+# Prepare MASTER_DIR
 
-cp -Rf ${PROACTIVE_DIR} ${TMP_DIR}
+cp -Rf ${ORIG_DIR} ${MASTER_DIR} || die "Failed to create MASTER_DIR"
 
+# Check that serial version UIDs match the version. Missing serial version UID are not checked
 cd ${TMP_DIR} || warn_and_exit "Cannot move in ${TMP_DIR}"
-if [ "$(find src/ -name "*.java" | xargs grep serialVersionUID | grep -v `echo $VERSION | cut -d'.' -f1,2 --output-delimiter ""` | wc -l)" -gt 0 ] ; then
+if [ "$(find ${MASTER_DIR}/src/ -name "*.java" | xargs grep serialVersionUID | grep -v `echo $VERSION | cut -d'.' -f1,2 --output-delimiter ""` | wc -l)" -gt 0 ] ; then
 	if [ -z "${RELAX}" ] ; then
 		warn_and_exit " [E] serialVersionUID are NOT defined"
 	fi
 fi
 
 # Check Main.PA_VERSION
-echo ${VERSION} | grep -q $(cat src/Core/org/objectweb/proactive/Main.java | grep 'static final private String PA_VERSION =' | cut -d '"' -f2)
+echo ${VERSION} | grep -q $(cat ${MASTER_DIR}/src/Core/org/objectweb/proactive/Main.java | grep 'static final private String PA_VERSION =' | cut -d '"' -f2)
 if [ "$?" != 0 ] ; then
 	warn_and_exit " [E] Bad version in Main.PA_VERSION should match ${VERSION}"
 fi
 
-# Update Main.PA_VERSION if needed
-sed -i  "s%\(static final private String PA_VERSION = \"\).\+\"%\1${VERSION}\"%" src/Core/org/objectweb/proactive/Main.java
-
-
-cd compile || warn_and_exit "Cannot move in compile"
-./build clean
-./build -Dversion="${VERSION}" deploy.all || warn_and_exit " [E] build failed (code) "
-./build -Dversion="${VERSION}" doc.ProActive.manualPdf || warn_and_exit " [E] build failed (documentation)"
-cd ..
-
-
-OUTPUT=$(${JAVA_HOME}/bin/java -cp dist/lib/ProActive.jar org.objectweb.proactive.api.PAVersion)
+# Update Main.PA_VERSION with the version tag if needed
+sed -i  "s%\(static final private String PA_VERSION = \"\).\+\"%\1${VERSION}\"%" ${MASTER_DIR}/src/Core/org/objectweb/proactive/Main.java
+(cd ${MASTER_DIR} ; ./compile/build deploy.all)
+OUTPUT=$(${JAVA_HOME}/bin/java -cp ${MASTER_DIR}/dist/lib/ProActive.jar org.objectweb.proactive.api.PAVersion)
 if [ "${OUTPUT}" != "${VERSION}" ] ; then
 	warn_and_exit " [E] bad release version number: $OUTPUT"
 fi
 
-cd ${TMP_DIR} || warn_and_exit "Cannot move in ${TMP_DIR}"
-echo " [i] Clean"
 
-# Subversion
-find . -type d -a -name ".svn" -exec rm -Rf {} \;
+# Clean extra files
+(
+	cd ${MASTER_DIR} 
+	./compile/build clean
+	# Subversion
+	find . -type d -a -name ".svn" -exec rm -Rf {} \;
+	# Git
+	rm -Rf .git
+	# Remove non GPL stuff
+	rm -Rf ./compile/lib/clover.*
+	# Remove useless parts of ProActive
+	rm ./doc/src/ProActiveRefBook.doc
+	find . -type f -a -name "*.svg" -exec rm {} \; # svg are converted in png by hands
+)
 
-# Git
-rm -Rf .git
+###########
+# Build Source artifact
 
-# Remove useless parts of ProActive
-rm ./doc/src/ProActiveRefBook.doc
-rm -Rf lib/client.jar dist/lib/client.jar
-find . -type f -a -name "*.svg" -exec rm {} \; # svg are converted in png by hands
+SRC_NAME=ProActiveProgramming-${VERSION}_core_src
+SRC_DIR=${WORK_DIR}/${SRC_NAME}
 
-# Remove non GPL stuff
-rm -Rf ./compile/lib/clover.*
+cp -r ${MASTER_DIR} ${SRC_DIR} || die "Failed to create SRC_DIR"
+(
+	cd ${SRC_DIR} || die
+	rm -Rf ./classes
+	rm -Rf ./dist
+	rm -Rf ./examples/calcium # save 12MB
+)
 
-# Remove temporary files
-rm compile/junit*properties
-rm -Rf classes/
-rm -Rf doc/ic2d
+# Publish artifacts
+(
+	cd ${WORK_DIR}
+	tar cvfz ${ARTIFACT_DIR}/${SRC_NAME}.tar.gz ${SRC_NAME}
+	zip -r   ${ARTIFACT_DIR}/${SRC_NAME}.zip    ${SRC_NAME}
+)
 
-sed -i "s/{version}/$VERSION/" README.txt
 
-cd ${TMP}
-tar cvfz ${DIR_NAME}.tar.gz ${DIR_NAME}
-zip -r   ${DIR_NAME}.zip    ${DIR_NAME}
+###########
+# Build Bin artifact
 
+BIN_NAME=ProActiveProgramming-${VERSION}_core_bin
+BIN_DIR=${WORK_DIR}/${BIN_NAME}
+
+cp -r ${MASTER_DIR} ${BIN_DIR} || die "Failed to create BIN_DIR"
+(
+	cd ${BIN_DIR} || die
+	./compile/build -Dversion="${VERSION}" deploy.all              || die "build failed (code) "
+	./compile/build -Dversion="${VERSION}" doc.ProActive.manualPdf || die "build failed (documentation)"
+	rm -Rf ./classes
+	rm -Rf ./compile
+	rm -Rf ./dev
+	rm -Rf ./doc/toolchain ./doc/src
+	rm -Rf ./examples/calcium # save 12MB
+	rm -Rf ./lib
+	rm -Rf ./scripts
+	rm -Rf ./src
+)
+
+# Publish artifacts
+(
+	cd ${WORK_DIR}
+	tar cvfz ${ARTIFACT_DIR}/${BIN_NAME}.tar.gz ${BIN_NAME}
+	zip -r   ${ARTIFACT_DIR}/${BIN_NAME}.zip    ${BIN_NAME}
+)
+
+
+
+###########
+# Cleanup
+
+rm -Rf ${WORK_DIR}
