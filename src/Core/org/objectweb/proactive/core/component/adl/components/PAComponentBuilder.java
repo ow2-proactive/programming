08b39e289564adc6d18ff3a2ae2118c2ae61010d
cruz <cruz@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-12-29 18:56:50 +0000

    + Extended implementation of PrimitiveComponentCompiler and PAComponentBuilder, to add tasks for including NF components in the membrane.
    + StartTask implemented to start the membranes after all manipulations.
    
    + PACompiler extended to include the new PAPrimitiveComponentCompiler
    
    + MembraneController added to default-component-config.xml controller configuration file, so that it's always created.
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Component_ExtendedFactory@21379 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/component/adl/components/PAComponentBuilder.java
+++ src/Core/org/objectweb/proactive/core/component/adl/components/PAComponentBuilder.java
@@ -36,26 +36,46 @@
  */
 package org.objectweb.proactive.core.component.adl.components;
 
+import java.util.Map;
+
+import org.apache.log4j.Logger;
 import org.etsi.uri.gcm.util.GCM;
 import org.objectweb.fractal.adl.components.ComponentBuilder;
 import org.objectweb.fractal.api.Component;
+import org.objectweb.fractal.api.NoSuchInterfaceException;
+import org.objectweb.proactive.core.component.Utils;
+import org.objectweb.proactive.core.component.control.PAMembraneController;
+import org.objectweb.proactive.core.component.identity.PAComponent;
+import org.objectweb.proactive.core.util.log.Loggers;
+import org.objectweb.proactive.core.util.log.ProActiveLogger;
 
 
 /**
  * A ProActive based implementation of the {@link ComponentBuilder} interface.
  * This implementation uses the Fractal API to add and start components.
  * It slightly differs from the FractalComponentBuilder class : the name of the component
- * is not specified in this addition operation, but when the component is instantiated.
+ * is not specified in this addition operation, but when the component is instantiated. <br/><br/>
  *
+ * The addComponent method is extended to discriminate functional subcomponents (that go in the functional content)
+ * from NF components (that go in the membrane).<br/><br/>
+ * 
+ * It also includes a method to explicitly start the membrane, so that the functional lifecycle can be
+ * properly started later.
+ * 
  * @author The ProActive Team
  *
  */
-public class PAComponentBuilder implements ComponentBuilder {
+public class PAComponentBuilder implements PAComponentBuilderItf {
+	
+	protected static final Logger logger = ProActiveLogger.getLogger(Loggers.COMPONENTS_ADL);
+	
     // --------------------------------------------------------------------------
     // Implementation of the ComponentBuilder interface
     // --------------------------------------------------------------------------
+	@Override
     public void addComponent(final Object superComponent, final Object subComponent, final String name,
             final Object context) throws Exception {
+    	
         GCM.getContentController((Component) superComponent).addFcSubComponent((Component) subComponent);
         // as opposed  to the standard fractal implementation, we do not set
         // the name of the component here because :
@@ -67,7 +87,81 @@ public class PAComponentBuilder implements ComponentBuilder {
         //    } catch (NoSuchInterfaceException ignored) {
         //    }
     }
+	
+	/**
+	 * Add the subComponent into the superComponent, either in the functional content,
+	 * or in the membrane.
+	 */
+	@Override
+	public void addComponent(Object superComponent, Object subComponent,
+			String name, boolean isFunctional, Map<Object, Object> context)
+			throws Exception {
+		
+		//DEBUG
+		String superComponentName = ((PAComponent) superComponent).getComponentParameters().getName();
+		String subComponentName = ((PAComponent) subComponent).getComponentParameters().getName();
+		logger.debug("[PAComponentBuilder] Adding "+ (isFunctional?"F":"NF") + " component " + subComponentName + " to " + superComponentName );
+		//--DEBUG
+		
+		if(isFunctional) {
+			// use the ContentController
+			GCM.getContentController((Component) superComponent).addFcSubComponent((Component) subComponent);
+		}
+		else {
+			// use the MembraneController
+			try {
+				// membrane of superComponent must be stopped
+				PAMembraneController pamc = Utils.getPAMembraneController((Component)superComponent);
+				pamc.stopMembrane();
+				//membrane of subComponent must be started (why?)
+				PAMembraneController subPamc = Utils.getPAMembraneController((Component)subComponent);
+				subPamc.startMembrane();
+				
+				logger.debug("[PAComponentBuilder] Membrane state-sup: "+ pamc.getMembraneState());
+				logger.debug("[PAComponentBuilder] Membrane state-sub: "+ subPamc.getMembraneState());
+				
+				// add the NF component
+				pamc.nfAddFcSubComponent((Component) subComponent);
+				
+			} catch (NoSuchInterfaceException nsie) {
+				logger.debug("[PAComponentBuilder] NOT FOUND Membrane Controller in "+ superComponentName);
+			}
 
+		}
+		
+	}
+
+    /**
+     * Nothing
+     */
     public void startComponent(final Object component, final Object context) throws Exception {
+    	
     }
+
+    /**
+     * Start the membrane of the component. 
+     * This method should be called after all the manipulations inside the membrane have been made.
+     */
+	@Override
+	public void startMembrane(final Object component, final Map<Object, Object> context) throws Exception {
+		
+		// DEBUG
+		String componentName = ((PAComponent) component).getComponentParameters().getName();
+		logger.debug("[PAComponentBuilder] Starting membrane of "+ componentName);
+		//--DEBUG
+		
+		try {
+			// start the membrane
+			PAMembraneController pamc = Utils.getPAMembraneController((Component)component);
+			pamc.startMembrane();
+			
+		} catch (NoSuchInterfaceException e) {
+			logger.debug("[PAComponentBuilder] NOT FOUND Membrane Controller in "+ componentName);
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+
 }
---
+++ src/Core/org/objectweb/proactive/core/component/adl/components/PAComponentBuilderItf.java
@@ -0,0 +1,62 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2011 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+package org.objectweb.proactive.core.component.adl.components;
+
+import java.util.Map;
+
+import org.objectweb.fractal.adl.components.ComponentBuilder;
+
+/**
+ * The {@link PAComponentBuilderItf} extends the {@link ComponentBuilder} interface,
+ * to indicate if the subcomponent to add is functional or non-functional.<br/>
+ * 
+ * The implementation may, so, determine if the subcomponent is added on the functional content
+ * (ContentController) or in the membrane (MembraneController).
+ * 
+ * It also includes a method to explicitly start the membrane, so that the functional lifecycle can be
+ * properly started later.
+ * 
+ * @author The ProActive Team
+ *
+ */
+public interface PAComponentBuilderItf extends ComponentBuilder {
+	
+	void addComponent (Object superComponent, Object subComponent, String name, boolean isFunctional, Map<Object, Object> context) throws Exception;
+	
+	void startMembrane (Object component, Map<Object, Object> context) throws Exception;
+
+}
