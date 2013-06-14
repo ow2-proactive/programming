2ee017463800a44969fa9f9409caa946bc09f6b4
fviale <fabien.viale@activeeon.com>
2013-06-14 18:38:39 +0200

    formatting
    Change-Id: I9adff37fa65f2121e7a7f474f0c51eb6b017c081

--- src/Core/org/objectweb/proactive/core/runtime/ProActiveRuntimeImpl.java
+++ src/Core/org/objectweb/proactive/core/runtime/ProActiveRuntimeImpl.java
@@ -184,7 +184,7 @@ public class ProActiveRuntimeImpl extends RuntimeRegistrationEventProducerImpl i
                 }
 
             } catch (Exception e) {
-                logger.fatal("Error while initializing ProActive Runtime",e);
+                logger.fatal("Error while initializing ProActive Runtime", e);
                 throw new RuntimeException(e);
             }
             return proActiveRuntime;
@@ -394,7 +394,6 @@ public class ProActiveRuntimeImpl extends RuntimeRegistrationEventProducerImpl i
             runtimeLogger.error("Parent runtime already set!");
         }
     }
-
 
     public void registerDescriptor(String url, ProActiveDescriptorInternal pad) {
         this.descriptorMap.put(url, pad);
