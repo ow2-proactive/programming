ed3386fd04baa8065a17a7d0696b071939507c59
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2013-06-28 17:04:00 +0200

    PALIGHT-27: Remove old trywithcatch
    Change-Id: Ib002b402eda8b39cfd504e5ba06a70de5444451b

--- bin/trywithcatch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo
-echo --- ProActive TryWithCatch annotator -----------------------
-
-workingDir=`dirname $0`
-. $workingDir/env.sh
-
-$JAVACMD trywithcatch.TryWithCatch "$@"
