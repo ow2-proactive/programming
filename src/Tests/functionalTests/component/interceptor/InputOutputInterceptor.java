59db4f2e24488f869a2b7dd4bfba45a48e497840
bsauvan <bastien.sauvan@inria.fr>
2013-07-15 16:31:57 +0200

    PROACTIVE-1283 : Format of controller interface names is not checked. Fix test.

--- src/Tests/functionalTests/component/interceptor/InputOutputInterceptor.java
+++ src/Tests/functionalTests/component/interceptor/InputOutputInterceptor.java
@@ -37,7 +37,7 @@
 package functionalTests.component.interceptor;
 
 public interface InputOutputInterceptor extends InputInterceptor1, OutputInterceptor1 {
-    public static final String INPUT_OUTPUT_INTERCEPTOR_NAME = "input-output-interceptor";
+    public static final String INPUT_OUTPUT_INTERCEPTOR_NAME = "input-output-interceptor-controller";
     public static final String AFTER_INPUT_INTERCEPTION = " - after-input-interception-" +
         INPUT_OUTPUT_INTERCEPTOR_NAME + " - ";
     public static final String BEFORE_INPUT_INTERCEPTION = " - before-input-interception-" +
